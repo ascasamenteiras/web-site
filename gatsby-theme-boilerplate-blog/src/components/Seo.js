bc49b713d84c3290344f02be997e03d2dab43b95
Milton ' Kiko ' Bolonha <miltonbolonha@gmail.com>
2023-01-08 07:23:36 -0300

    Update Seo.js

--- gatsby-theme-boilerplate-blog/src/components/Seo.js
+++ gatsby-theme-boilerplate-blog/src/components/Seo.js
@@ -120,10 +120,6 @@ const Seo = ({
         ) : (
           ""
         )}
-        const isBrowser = typeof window !== "undefined"
-        if (isBrowser) {
-          return <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10951493353"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-10951493353'); </script>
-        }       
 
       </Helmet>
       <SchemaOrgContainer
