dc18eb5961952acd25621b64248adc20843acfd7
Milton ' Kiko ' Bolonha <miltonbolonha@gmail.com>
2023-01-08 06:59:58 -0300

    Second GTag

--- gatsby-theme-boilerplate-blog/src/components/Seo.js
+++ gatsby-theme-boilerplate-blog/src/components/Seo.js
@@ -120,6 +120,8 @@ const Seo = ({
         ) : (
           ""
         )}
+        <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10951493353"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-10951493353'); </script>
+
       </Helmet>
       <SchemaOrgContainer
         schemaType={schemaType}
