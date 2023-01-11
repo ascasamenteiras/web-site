02c270721fc02192dbac46edca7638652147edd3
miltonbolonha <miltonbolonha@gmail.com>
2023-01-11 18:10:11 -0300

    api update

--- engine/src/api/sendtest.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === `POST`) {
-    res.send(`I am POST3`);
-  } else {
-    res.send(`I am GET3`);
-    // Handle other methods or return error
-  }
-}
